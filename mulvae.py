9bff1e7a3a75b41d908778593256946755a159c9
sharon200102 <sharon200102@gmail.com>
2020-11-02 14:57:14 +0200

    first upload

---
+++ mulvae.py
@@ -0,0 +1,13 @@
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+
+class MultipleVAE(nn.Module):
+    def __init__(self,xyvae,xvae,yvae):
+        self,xyvae=xyvae
+        self.xvar=xyvae
+        self.yvar=yvae
+    def forward(self, x,y):
+        
+
---
+++ stdvae.py
@@ -0,0 +1,65 @@
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+
+
+class StandardVAE(nn.Module):
+    def __init__(self, layers_structure, activation_fn):
+        """
+        :param layers_structure: An iterable which will form the structure of the encoder, As a result, the structure
+        of the decoder will be symmetrical.
+
+        :param activation_fn: A Pointer to the activation function that will be used between the layers.
+        """
+        super(StandardVAE, self).__init__()
+        self.activation_fn = activation_fn
+        reversed_layers_structure = reversed(
+            layers_structure)  # the reversed list will form the structure of the decoder.
+        self.encoding_layers = nn.ModuleList(
+            [nn.Linear(layers_structure[i], layers_structure[i + 1]) for i in range(0, len(layers_structure) - 2)])   # Don't create the last layer because it needs to be split.
+
+        # Layers for the mean and the variance of the normal distribution.
+
+        self.mu_layer = nn.Linear(layers_structure[-2], layers_structure[-1])
+
+        self.logvar_layer = nn.Linear(layers_structure[-2], layers_structure[-1])
+
+        self.decoding_layers = nn.ModuleList(
+            [nn.Linear(reversed_layers_structure[i], reversed_layers_structure[i + 1]) for i in
+             range(0, len(reversed_layers_structure) - 1)])
+
+    def encode(self, x):
+        """
+        :param x The input to the network, The function will output the corresponding mu, and logvar.
+        """
+        for encoding_layer in self.encoding_layers:
+            x = self.activation_fn(encoding_layer(x))
+        return self.mu_layer(x),self.logvar_layer(x)
+
+
+    def reparameterize(self, mu, logvar):
+        """
+
+        :param mu The first output of the encoding function, represents the mean of the latent distribution
+        :param logvar: The second output of the encoding function, will be transformed into the distribution variance.
+        :return: The latent representation of the input of the network, using the parameterization trick.
+        """
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, z):
+        """
+
+        :param z: the latent representation of the input which is the output of the reparameterize function.
+        :return:
+        """
+        for decoding_layer in self.decoding_layers:
+            z = self.activation_fn(decoding_layer(z))
+        return torch.sigmoid(z)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, len(x)))   # self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), z, mu, logvar
