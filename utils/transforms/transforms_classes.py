2d9b4b8b9cf86836f05b28a8205a89a31d84adda
sharon200102 <sharon200102@gmail.com>
2020-11-14 17:20:35 +0200

    rearranging the utils files

---
+++ utils/transforms/transforms_classes.py
@@ -0,0 +1,15 @@
+import torch
+from Projects.GDM.code.data_classes import patient_samples_single_tp
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        sample_dict_copy = dict(sample.patient_dict)
+        if sample_dict_copy.get('FIELD0', None) is not None:
+            sample_dict_copy['FIELD0'] = torch.tensor(sample.patient_dict['FIELD0'].values.astype('float64')).type(torch.FloatTensor)
+        if sample_dict_copy.get('FIELD1', None) is not None:
+            sample_dict_copy['FIELD1'] = torch.tensor(sample.patient_dict['FIELD1'].values.astype('float64')).type(torch.FloatTensor)
+        return patient_samples_single_tp(sample_dict_copy)
+
